c9a86aea58cf7167af4ebdb57b00d4bf60b1325f
IT24102303 <it24102303@my.sliit.lk>
2025-09-26 14:17:13 +0530

    Add files via upload

---
+++ Lab Sheet 08/IT24102303lab8.R
@@ -0,0 +1,46 @@
+setwd("C:\\Users\\it24102303\\Desktop\\IT24102303\\Lab 08-20250926")
+
+#setting the directory
+setwd("C:/Users/IT24101020/Desktop/it24101020")
+
+#importing the data set
+data <- read.table("Exercise - LaptopsWeights.txt",header=TRUE)
+fix(data)
+attach(data)
+
+# Question 01
+# Calculating population mean & standard deviation
+pop_mean_laptop <-mean(Weight.kg.)
+pop_sd_laptop <-sd(Weight.kg.)
+
+# Question 02
+# Creating null vectors to store sample data sets
+samples_laptop <- c()
+n_laptop <- c()
+
+# Drawing 25 sample of size 6 
+for (i in 1:25){
+  s_laptop <- sample(Weight.kg.,6,replace = TRUE)
+  samples_laptop <-cbind(samples_laptop,s_laptop)
+  n_laptop <-c(n_laptop,paste('S',i))
+}
+
+#Assigning column names
+colnames(samples_laptop) =n_laptop
+
+#calculating sample means and standard deviations
+s.mean_laptop <- apply(samples_laptop,2,mean)
+s.sd_laptop <- apply(samples_laptop,2,sd)
+
+# Question 03
+#calculating the mean and standard deviation of the sample means
+mean_of_s_means <- mean(s.mean_laptop)
+sd_of_s_means <- sd(s.mean_laptop)
+
+# comparing the values
+pop_mean_laptop
+mean_of_s_means
+
+pop_sd_laptop
+sd_of_s_means
+
